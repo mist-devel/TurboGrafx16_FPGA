6a568f66b6011582c31755640685e8be4c7b48ce
Sergiy Dvodnenko <dvodnenkos@gmail.com>
2024-11-20 12:56:05 +0200

    VDC: fix line break on HSYNC signal (Outrun)
    (cherry picked from commit 09869ee6b17898bb5671be5beb1e8cc24f44c11e)

--- rtl/pce_top_extram.vhd
+++ rtl/pce_top_extram.vhd
@@ -213,11 +213,10 @@ component CODES is
 	);
 end component;
 
-signal VCE_HS_F, VCE_HS_R, VCE_VS_F, VCE_VS_R: std_logic;
+signal VCE_HSYNC_F, VCE_HSYNC_R, VCE_VSYNC_F, VCE_VSYNC_R: std_logic;
 signal CLR_A	   : std_logic_vector(12 downto 0);
 signal CLR_WE		: std_logic;
 
-signal VCE_DCC		: std_logic_vector(1 downto 0);
 signal VDC0_BORDER: std_logic;
 signal VDC0_GRID	: std_logic_vector(1 downto 0);
 signal CPU_PRE_RD	: std_logic;
@@ -333,7 +332,6 @@ CPU_CLKEN <= CPU_CE when rising_edge( CLK );
 VIDEO_CE <= VDC_CLKEN;
 VIDEO_VS <= not VS_N;
 VIDEO_HS <= not HS_N;
-VIDEO_DCC <= VCE_DCC;
 
 VCE : entity work.huc6260
 port map(
@@ -351,10 +349,14 @@ port map(
 	-- VDC Interface
 	COLNO		=> VDC_COLNO,
 	CLKEN		=> VDC_CLKEN,
+	HSYNC_F	=> VCE_HSYNC_F,
+	HSYNC_R	=> VCE_HSYNC_R,
+	VSYNC_F	=> VCE_VSYNC_F,
+	VSYNC_R	=> VCE_VSYNC_R,
 	CLKEN_FS => VIDEO_CE_FS,
 	RVBL		=> ReducedVBL,
-	DCC		=> VCE_DCC,
-	
+	DCC      => VIDEO_DCC,
+
 	GRID_EN	=> GRID_EN,
 	BORDER_EN=> BORDER_EN,
 	BORDER	=> BORDER,
@@ -368,12 +370,7 @@ port map(
 	VS_N		=> VS_N,
 	HS_N		=> HS_N,
 	HBL		=> VIDEO_HBL,
-	VBL		=> VIDEO_VBL,
-	
-	HS_F		=> VCE_HS_F,
-	HS_R		=> VCE_HS_R,
-	VS_F		=> VCE_VS_F,
-	VS_R		=> VCE_VS_R
+	VBL		=> VIDEO_VBL
 );
 
 VRAM0_RD <= VRAM0_READ and not VRAM0_ADDR(15);
@@ -402,11 +399,10 @@ port map(
 
 	-- VCE Interface
 	DCK_CE	=> VDC_CLKEN,
-	DCC		=> VCE_DCC,
-	HS_F		=> VCE_HS_F,
-	HS_R		=> VCE_HS_R,
-	VS_F		=> VCE_VS_F,
-	VS_R		=> VCE_VS_R,
+	HSYNC_F	=> VCE_HSYNC_F,
+	HSYNC_R	=> VCE_HSYNC_R,
+	VSYNC_F	=> VCE_VSYNC_F,
+	VSYNC_R	=> VCE_VSYNC_R,
 	VD			=> VDC0_COLNO,
 	
 	BORDER	=> VDC0_BORDER,
@@ -454,11 +450,10 @@ generate_SGX: if (LITE = 0) generate begin
 
 		-- VCE Interface
 		DCK_CE	=> VDC_CLKEN,
-		DCC		=> VCE_DCC,
-		HS_F		=> VCE_HS_F,
-		HS_R		=> VCE_HS_R,
-		VS_F		=> VCE_VS_F,
-		VS_R		=> VCE_VS_R,
+		HSYNC_F	=> VCE_HSYNC_F,
+		HSYNC_R	=> VCE_HSYNC_R,
+		VSYNC_F	=> VCE_VSYNC_F,
+		VSYNC_R	=> VCE_VSYNC_R,
 		VD			=> VDC1_COLNO,
 		--GRID		=> VDC1_GRID,
 		
@@ -486,7 +481,7 @@ generate_SGX: if (LITE = 0) generate begin
 		DI			=> CPU_DO,
 		DO 		=> VPC_DO,
 		
-		HS_F		=> VCE_HS_F,
+		HS_F		=> VCE_HSYNC_F,
 		VDC0_IN  => VDC0_COLNO,
 		VDC1_IN  => VDC1_COLNO,
 		VDC_OUT  => VDC_COLNO,
